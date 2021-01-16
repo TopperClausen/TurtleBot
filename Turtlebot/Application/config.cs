41630def607f317984db9836f9a0e30b77a80e9d
ToppeRTheReal <topperhdoriginal@gmail.com>
2021-01-16 03:16:46 +0100

    fixed issue

--- Turtlebot/Application/config.cs
+++ Turtlebot/Application/config.cs
@@ -1,7 +1,7 @@
 namespace Application {
     public static class Config {
 
-        public static string DiscordToken = "NzczNjYxODY5MDM4MTA4Njgy.X6Meww.ANAGhzG_avRGx7MECMSwMxpvauE";
+        public static string DiscordToken = "token goes here";
 
     }
 }
