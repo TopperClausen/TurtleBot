d655b4b2f629710fc4890678c23b3056fe2e365a
ToppeRTheReal <topperhdoriginal@gmail.com>
2021-01-16 03:06:31 +0100

    cfg

--- Turtlebot/Application/config.cs
+++ Turtlebot/Application/config.cs
@@ -1,7 +1,7 @@
 namespace Application {
     public static class Config {
 
-        public static string DiscordToken = "NzczNjYxODY5MDM4MTA4Njgy.X6Meww.G9mFoXdu6qJDSbl4CJ0jhkHuo9A";
+        public static string DiscordToken = "";
 
     }
 }
